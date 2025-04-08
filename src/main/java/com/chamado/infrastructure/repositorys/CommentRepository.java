12c66f2cd19b22ec7227b71d620907c36365ef47
Ariel Bertoletti Ribeiro <arielbertolettiribeiro@gmail.com>
2025-04-08 20:19:48 -0300

    feta: add repositorys

---
+++ src/main/java/com/chamado/infrastructure/repositorys/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.chamado.infrastructure.repositorys;
+
+import com.chamado.domain.entities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
